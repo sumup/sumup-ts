5d9c5fad4418873aaaeb7477de8f5c4c073c4674
Matouš Dzivjak <matousdzivjak@gmail.com>
2025-02-16 20:24:54 +0100

    chore: fix dts, bundle dist
    Fix dts files, bundled version didn't work.
    Simplify dist/ by bundling CommonJS and ES Next
    files. Also updates package.json accordingly.

--- examples/checkout/index.ts
+++ examples/checkout/index.ts
@@ -10,6 +10,41 @@ async function main() {
 
   const merchant2 = await client.merchant.get().withResponse();
   console.info({ merchant2 });
+
+  const merchantCode = "MERCHANT_CODE";
+
+  const readers = await client.readers.list(merchantCode);
+  const reader = readers[0];
+
+  client.readers.createCheckout(merchantCode, reader.id, {
+    total_amount: {
+      currency: "EUR",
+      minor_unit: 100,
+      value: 500,
+    },
+  });
+
+  const checkout = await client.checkouts.create({
+    amount: 19,
+    checkout_reference: "CO746453",
+    currency: "EUR",
+    merchant_code: merchantCode,
+  });
+
+  // Collect customer's card data
+
+  const result = client.checkouts.process(checkout.id, {
+    card: {
+      cvv: "123",
+      expiry_month: "12",
+      expiry_year: "2023",
+      name: "Boaty McBoatface",
+      number: "4200000000000042",
+    },
+    payment_type: "card",
+  });
+
+  console.info({ result });
 }
 
 main();
